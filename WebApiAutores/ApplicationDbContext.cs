7776acc651eda31a7e5a46e17c34b17255c859f2
EMijango3 <mijango_3@hotmail.com>
2022-08-31 11:55:13 -0600

    Initial commit

---
+++ WebApiAutores/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        /*con DbSet<Autor> le indico que me cree una tabla en base alas propiedades de Autores */
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() {
+                new Autor(){ Id =1,Nombre = "felipe"},
+                new Autor(){ Id =2,Nombre = "Claudia"}
+            };
+        }
+    }
+}
